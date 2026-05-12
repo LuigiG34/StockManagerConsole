7b476313a81d9527d450b0499e5b46df753c9d4a
Luigi Gandemer <luigigandemer6@gmail.com>
2026-05-12 14:18:58 +0200

    Updated folder structure

--- src/com/stockmanager/Main.java
+++ src/com/stockmanager/Main.java
@@ -1,4 +1,6 @@
-public class App {
+package com.stockmanager;
+
+public class Main {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
     }
